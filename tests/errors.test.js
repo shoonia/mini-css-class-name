74692cdec7b59bc7724524e225e603c473465f10
shoonia <zaytsev126@gmail.com>
2020-02-01 17:29:28 +0200

    Check error message

--- tests/errors.test.js
+++ tests/errors.test.js
@@ -7,45 +7,54 @@ describe("Errors", () => {
     expect(run(null)).toThrow(TypeError);
   });
 
-  it("should be invalid prefix", () => {
-    expect(run({ prefix: null })).toThrow(TypeError);
-    expect(run({ prefix: 6 })).toThrow(TypeError);
+  it("should be invalid `prefix`", () => {
+    const msg = "mini-css-class-name: `prefix` must be a String";
+    expect(run({ prefix: null })).toThrowError(msg);
+    expect(run({ prefix: 6 })).toThrowError(msg);
   });
 
   it("should be invalid `prefix` char", () => {
-    expect(run({ prefix: "x--ß--" })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `prefix` and `suffix` can contain only the characters [a-zA-Z0-9], plus the hyphen (-) and the underscore (_);";
+    expect(run({ prefix: "x--ß--" })).toThrowError(msg);
   });
 
   it("should be invalid first `prefix` char", () => {
-    expect(run({ prefix: "5" })).toThrow(TypeError);
-    expect(run({ prefix: "-" })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `prefix` cannot start with a digit or hyphens";
+    expect(run({ prefix: "5" })).toThrowError(msg);
+    expect(run({ prefix: "-" })).toThrowError(msg);
   });
 
   it("should be invalid suffix", () => {
-    expect(run({ suffix: null })).toThrow(TypeError);
-    expect(run({ suffix: 0 })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `suffix` must be a String";
+    expect(run({ suffix: null })).toThrowError(msg);
+    expect(run({ suffix: 0 })).toThrowError(msg);
   });
 
   it("should be invalid `suffix` char", () => {
-    expect(run({ suffix: "--ß" })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `prefix` and `suffix` can contain only the characters [a-zA-Z0-9], plus the hyphen (-) and the underscore (_);";
+    expect(run({ suffix: "--ß" })).toThrowError(msg);
   });
 
   it("should be invalid hash", () => {
-    expect(run({ hash: null })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `hash` must be a Number";
+    expect(run({ hash: null })).toThrowError(msg);
   });
 
   it("should be invalid excludePattern", () => {
-    expect(run({ excludePattern: "" })).toThrow(TypeError);
-    expect(run({ excludePattern: 1 })).toThrow(TypeError);
+    const msg = "mini-css-class-name: `excludePattern` must be a RegExp";
+    expect(run({ excludePattern: "" })).toThrowError(msg);
+    expect(run({ excludePattern: 1 })).toThrowError(msg);
   });
 
   // NO ERROR
 
   it("shouldn't be error, excludePattern is null", () => {
-    miniClassName({ excludePattern: null });
+    const cb = miniClassName({ excludePattern: null });
+    expect(typeof cb).toBe("function");
   });
 
   it("shouldn't be error, excludePattern is RegExp", () => {
-    miniClassName({ excludePattern: /regex/ });
+    const cb = miniClassName({ excludePattern: /regex/ });
+    expect(typeof cb).toBe("function");
   });
 });
